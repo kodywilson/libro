7b1b63d5b491e3aeffaa725747d9205a2ad66f2d
Kody Wilson <kodywilson@gmail.com>
2014-08-21 22:18:20 -0700

    Fixes #6 Changed book controller to only allow show and index when not logged in and also updated view to only show new book button when logged into the app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!, :except => [:show, :index]
   # GET /books
   # GET /books.json
   def index
